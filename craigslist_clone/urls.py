3c049b790dbb8b0b5890f9938da528b0994b1f4d
PratikCods <sandyhammer31@gmail.com>
2020-08-07 15:19:54 +0530

    Added frontend and requirements

--- craigslist_clone/urls.py
+++ craigslist_clone/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('home/',include('my_app.urls')),
+    path('',include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render,reverse
 
 def home(request):
     return render(request,'my_app/base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    return render(request,'my_app/new_search.html',{'search':search})
